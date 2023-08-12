a1f50e0e9682448543b4dd20eb6f036e486c36fb
ณวพน ลีลานวลิขิต <nawapon@Macintosh.local>
2023-08-12 18:35:29 +0700

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('role')->default('STUDENT')->comment('UserRoleEnum');
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number')->nullable();
+            $table->string('contact_link')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2023_08_12_181242_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->date('created_at');
+            $table->date('updated_at');
+            $table->text('event_name');
+            $table->double('fund');
+            $table->string('curreny')->default('THB')->comment('CurrencyEnum');
+            $table->text('description');
+            $table->datetime('date');
+            $table->string('status');
+            $table->integer('attendee_capacity');
+            $table->text('disapproval_reasons');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
